9948476001e8e26fb3b38ffb2ce9626778a06536
SQIdward <69307539+Squidward-mv@users.noreply.github.com>
2021-12-07 19:42:16 +0300

    Update ex12.cpp

--- ex12.cpp
+++ ex12.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <stack>
  
 using namespace std;
+
+stack<char> newStack;
 
 bool checker(const char * s, int counter = 0, int counterSquare = 0, int counterFigure = 0){
     if ((counter < 0) || (counterSquare < 0) || (counterFigure < 0)) return false;
 
     for( ; *s; ++s)
         switch(*s){
-            case '(': return checker(s + 1, counter + 1, counterSquare, counterFigure);
-            case ')': return checker(s + 1, counter - 1, counterSquare, counterFigure);
-            case '[': return checker(s + 1, counter, counterSquare + 1, counterFigure);
-            case ']': return checker(s + 1, counter, counterSquare - 1, counterFigure);
-            case '{': return checker(s + 1, counter, counterSquare, counterFigure + 1);
-            case '}': return checker(s + 1, counter, counterSquare, counterFigure - 1);
+            case '(': 
+                newStack.push(*s);
+                return checker(s + 1, counter + 1, counterSquare, counterFigure);
+            case ')':
+                if(newStack.top() != '('){
+                    return false;
+                }
+                else newStack.pop();
+                return checker(s + 1, counter - 1, counterSquare, counterFigure);
+            case '[': 
+                newStack.push(*s);
+                return checker(s + 1, counter, counterSquare + 1, counterFigure);
+            case ']':
+                if(newStack.top() != '['){
+                    return false;
+                }
+                else newStack.pop();
+                return checker(s + 1, counter, counterSquare - 1, counterFigure);
+            case '{': 
+                newStack.push(*s);
+                return checker(s + 1, counter, counterSquare, counterFigure + 1);
+            case '}':
+                if(newStack.top() != '{'){
+                    return false;
+                }
+                else newStack.pop();
+                return checker(s + 1, counter, counterSquare, counterFigure - 1);
         }
         
     return ((counter || counterSquare || counterSquare) == 0);
