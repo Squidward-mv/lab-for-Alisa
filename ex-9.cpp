76cbbddbbdb3d687032243c0302f3b0870a42ab5
SQIdward <69307539+Squidward-mv@users.noreply.github.com>
2021-12-12 16:22:30 +0300

    Update ex-9.cpp

--- ex-9.cpp
+++ ex-9.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
+const string path = "file.txt";
+
 struct student{
     string lastName;
-    int course;
+    string course;
 };
 
 int main(){
+    ifstream file;
+    file.open(path);
+    
     int n;
     cout << "Введите количество студентов: ";
     cin >> n;
     
     student students[n];
     
-    for(int i = 0; i < n; i++){
-        cout << "Введите фамилию студента: ";
-        cin >> students[i].lastName;
-        cout << "Введите курс студента: ";
-        cin >> students[i].course;
+    string letter;
+    int i = 0;
+    
+    while(getline(file, letter)){
+        if(letter >= "1" && letter <= "9"){
+            students[i].course = letter;
+            cout << letter << endl;
+        }
+        else{
+            students[i].lastName = letter;
+            cout << letter << endl;
+        }
+        if(!students[i].lastName.empty() && !students[i].course.empty()){
+            i++;
+        }
     }
+    
+    file.close();
     
     string hypeLastName = " ";
     int countStudents = 0;
@@ -28,7 +46,7 @@ int main(){
     for(int i = 0; i < n; i++){
         int min = 1;
         for(int j = 1; j < n; j++){
-            if((students[j].course == 1) && (students[j].lastName == students[j - 1].lastName)){
+            if((students[j].course == "1") && (students[j].lastName == students[j - 1].lastName)){
                 min++;
                 hypeLastName = students[j].lastName;
             }
@@ -40,7 +58,7 @@ int main(){
     
     cout << "Самая популярная фамилия на первом курсе: " << hypeLastName << endl;
     
-    int num;
+    string num;
     cout << "Введите интересующий вас курс: ";
     cin >> num;
     
